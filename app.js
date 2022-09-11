063dba6905fe089e8975ed061af5b5009497252a
Mac <sophanpa@gmail.com>
2022-09-11 23:15:54 +0200

    Edit app.js

---
+++ app.js
@@ -0,0 +1,137 @@
+
+
+const addBook = document.querySelector(".add");
+addBook.addEventListener("click", ()=>{
+
+addBook.classList.add("no-click");
+document.querySelector(".modal").style.display = "flex";
+document.querySelector("#popup").classList.add("modal-bg");
+document.querySelector(".books").classList.add("modal-books");
+
+
+document.querySelector("#title").value = "";
+document.querySelector("#author").value = "";
+document.querySelector("#pages").value = "";
+document.querySelector("#check").checked = false;
+}) 
+
+const submit = document.querySelector("#submit");
+
+submit.addEventListener("click", ()=>{
+
+    if(document.querySelector("#title").value == "" || document.querySelector("#author").value == "" || document.querySelector("#pages").value == ""){
+       submit.type = "none"
+        
+        }else{
+                  const booksGrid = document.querySelector(".books");
+const book = document.createElement("div");
+const author = document.createElement("p");
+const bookName = document.createElement("p");
+const pages = document.createElement("p");
+const read = document.createElement("button");
+
+const remove = document.createElement("button")
+
+
+
+const t = document.querySelector("#title").value;
+const a = document.querySelector("#author").value;
+const p = document.querySelector("#pages").value;
+let r = document.querySelector("#check").checked;
+
+ function abook (a,n,p){
+   author.innerHTML = a; 
+   bookName.innerHTML = `" `+n+` "`;
+   pages.innerHTML = p;
+
+   if(r){
+    read.innerHTML = "Read";   
+    read.style.backgroundColor = "rgb(159, 255, 156)";
+   }else{
+        read.innerHTML = "Not read";   
+    read.style.backgroundColor = "rgb(255, 156, 156)";
+   }
+
+   
+}
+
+
+abook(a,t,p);
+
+
+
+remove.innerHTML = "Remove";
+
+book.appendChild(author);
+book.appendChild(bookName);
+book.appendChild(pages);
+book.appendChild(read);
+book.appendChild(remove);
+
+
+
+booksGrid.appendChild(book);
+book.classList.add("book");
+
+
+
+read.addEventListener("click", () => {
+    if(!r){
+        read.innerHTML = "Read";   
+        read.style.backgroundColor = "rgb(159, 255, 156)";
+      r = true;
+    } else{
+        read.innerHTML = "Not read";   
+        read.style.backgroundColor = "rgb(255, 156, 156)";
+        r = false;
+    }
+    
+   
+})
+
+remove.addEventListener("click", ()=>{
+    booksGrid.removeChild(book);
+})
+
+
+document.querySelector(".modal").style.display = "none";
+document.querySelector("#popup").classList.remove("modal-bg");
+document.querySelector(".books").classList.remove("modal-books");
+addBook.classList.remove("no-click");
+    
+
+    
+        } 
+
+
+
+
+})    
+
+
+
+
+
+
+
+
+let mg = document.querySelector(".modal-bg");
+
+
+window.onclick = function(event) {
+    if (event.target == document.querySelector(".modal-bg")) {
+        document.querySelector(".modal").classList.add("gone");
+        function meineFunktion() {
+           document.querySelector(".modal").style.display = "none";
+           document.querySelector("#popup").classList.remove("modal-bg");
+document.querySelector(".books").classList.remove("modal-books");
+addBook.classList.remove("no-click");
+document.querySelector(".modal").classList.remove("gone");
+          }
+          
+          setTimeout(meineFunktion, 500);
+       
+        
+
+    }
+  }
