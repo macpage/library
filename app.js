6558a90a3195df6bd987a8c5a589d25989633217
Mac <sophanpa@gmail.com>
2022-09-12 15:12:18 +0200

    Edit app.js

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 
+       const val = document.createElement("p");
+       val.innerHTML = "* Fill Empty!"
+       val.style.fontSize = "16px";
+       val.style.color = "red";
 
 const addBook = document.querySelector(".add");
 addBook.addEventListener("click", ()=>{
@@ -10,9 +14,16 @@ document.querySelector(".books").classList.add("modal-books");
 
 
 document.querySelector("#title").value = "";
+document.querySelector("#title").style.border = "none"
 document.querySelector("#author").value = "";
+document.querySelector("#author").style.border = "none"
 document.querySelector("#pages").value = "";
+document.querySelector("#pages").style.border = "none"
 document.querySelector("#check").checked = false;
+if(document.querySelector(".modal-main").childElementCount > 3){
+  document.querySelector(".modal-main").removeChild(val); 
+   }
+
 }) 
 
 const submit = document.querySelector("#submit");
@@ -20,9 +31,30 @@ const submit = document.querySelector("#submit");
 submit.addEventListener("click", ()=>{
 
     if(document.querySelector("#title").value == "" || document.querySelector("#author").value == "" || document.querySelector("#pages").value == ""){
-       submit.type = "none"
+
+       if(document.querySelector("#title").value == ""){
+        document.querySelector("#title").style.border = "2px solid red";
+       } else{
+        document.querySelector("#title").style.border = "none";
+       }
+       if(document.querySelector("#author").value == ""){
+        document.querySelector("#author").style.border = "2px solid red";
+       } else{
+        document.querySelector("#author").style.border = "none";
+       }
+       if(document.querySelector("#pages").value == ""){
+        document.querySelector("#pages").style.border = "2px solid red";
+       } else{
+        document.querySelector("#pages").style.border = "none";
+       }
+       
+       if(document.querySelector(".modal-main").childElementCount < 4){
+        document.querySelector(".modal-main").appendChild(val);
+       }
+       
+        }
         
-        }else{
+        else{
                   const booksGrid = document.querySelector(".books");
 const book = document.createElement("div");
 const author = document.createElement("p");
@@ -32,7 +64,11 @@ const read = document.createElement("button");
 
 const remove = document.createElement("button")
 
-
+read.style.fontWeight = "500";
+remove.style.fontWeight = "500";
+bookName.style.fontWeight = "500";
+author.style.fontWeight = "500";
+pages.style.fontWeight = "500";
 
 const t = document.querySelector("#title").value;
 const a = document.querySelector("#author").value;
@@ -42,7 +78,7 @@ let r = document.querySelector("#check").checked;
  function abook (a,n,p){
    author.innerHTML = a; 
    bookName.innerHTML = `" `+n+` "`;
-   pages.innerHTML = p;
+   pages.innerHTML = p + " pages";
 
    if(r){
     read.innerHTML = "Read";   
